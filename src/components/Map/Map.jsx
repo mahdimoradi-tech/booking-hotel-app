e3ab44ec7b572c227183b19e5ceaa11044e0a609
Mahdi Moradi <mahdimoradi.tech@gmail.com>
2026-07-22 21:03:06 +0330

    feat(map-current-postion): build a custom useGeoLocation hook to fetch the user's current GPS position

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,16 +2,27 @@ import { useEffect, useState } from "react";
 import { useHotels } from "../context/HotelsProvider";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { useSearchParams } from "react-router-dom";
+import useGeoLocation from "../../hooks/useGeoLocation";
 function Map() {
   const { hotels, isLoading } = useHotels();
   const [mapCenter, setMapCenter] = useState([51, 3]);
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  const {
+    getPosition,
+    isLoading: isLoadingPosition,
+    position: getCurrentPosition,
+  } = useGeoLocation();
 
   useEffect(() => {
     if (lat && lng) setMapCenter([lat, lng]);
   }, [lat, lng]);
+
+  useEffect(() => {
+    if (getCurrentPosition?.lat && getCurrentPosition?.lng)
+      setMapCenter([getCurrentPosition?.lat, getCurrentPosition?.lng]);
+  }, [getCurrentPosition]);
 
   return (
     <div className="mapContainer">
@@ -25,6 +36,9 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <button className="getLocation" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "use your location"}
+        </button>
         <ChangeCenter position={mapCenter} />
         {hotels.map((item) => (
           <Marker key={item.id} position={[item.latitude, item.longitude]}>
