369aad48e082143b690ab31b67d31130c5cd3790
Mahdi Moradi <mahdimoradi.tech@gmail.com>
2026-08-04 23:06:55 +0330

    ui(app-layout): build new layout for AppLayout component to toggle map veiw and hotels

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,15 +1,64 @@
+// import { Outlet } from "react-router-dom";
+// import Map from "../Map/Map";
+// import { useHotels } from "../context/HotelsProvider";
+
+// function AppLayout() {
+//   const { hotels } = useHotels();
+//   return (
+//     <div className="appLayout">
+//       <div className="sidebar">
+//         <Outlet />
+//       </div>
+//       <Map markerLocations={hotels} />
+//     </div>
+//   );
+// }
+
+// export default AppLayout;
+
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Map from "../Map/Map";
 import { useHotels } from "../context/HotelsProvider";
+import { HiOutlineMap } from "react-icons/hi2";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
 function AppLayout() {
   const { hotels } = useHotels();
+  const [isMapView, setIsMapView] = useState(false);
+
   return (
-    <div className="appLayout">
-      <div className="sidebar">
+    <div className="app-layout">
+      <div
+        className={`app-layout__sidebar ${
+          isMapView ? "app-layout__sidebar--hidden-mobile" : ""
+        }`}
+      >
         <Outlet />
       </div>
-      <Map markerLocations={hotels} />
+
+      <div
+        className={`app-layout__map ${
+          !isMapView ? "app-layout__map--hidden-mobile" : ""
+        }`}
+      >
+        <Map markerLocations={hotels} />
+      </div>
+
+      <button
+        className="app-layout__toggle-btn"
+        onClick={() => setIsMapView(!isMapView)}
+      >
+        {isMapView ? (
+          <>
+            <HiOutlineMenuAlt4 className="icon" /> List view
+          </>
+        ) : (
+          <>
+            <HiOutlineMap className="icon" /> Map view
+          </>
+        )}
+      </button>
     </div>
   );
 }
